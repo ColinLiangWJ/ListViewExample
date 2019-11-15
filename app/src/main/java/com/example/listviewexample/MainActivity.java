4405b44a96175cdeccab0e5323c7371b26e07478
ZhangRunHui <52392327+ZhangRunHui@users.noreply.github.com>
2019-11-15 09:05:31 +0800

    Endednclass

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -23,5 +23,9 @@ public class MainActivity extends AppCompatActivity  {
     }
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
-
+    public Player[] getPlayers(){
+        Player[] a;
+        String[] names={"11","22","33","44","55","66","77","88","99","1010","1111","1212","1313","1414","1515"};
+        return null;
+    }
 }

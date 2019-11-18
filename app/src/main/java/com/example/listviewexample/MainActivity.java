8a5d0d90410e6a55d558cbac0b2e602f11be5663
ZhangRunHui <52392327+ZhangRunHui@users.noreply.github.com>
2019-11-18 08:56:12 +0800

    Done initializing

--- app/src/main/java/com/example/listviewexample/MainActivity.java
+++ app/src/main/java/com/example/listviewexample/MainActivity.java
@@ -24,10 +24,15 @@ public class MainActivity extends AppCompatActivity  {
     //Todo  create a method getPlayers() that will create an array of 15 players and return the list of type List<Player>
     // choose successful sportsmen from different sports, make sure to fill Player class before starting
     public Player[] getPlayers(){
-        Player[] a;
+        Player[] a={};
         String[] names={"11","22","33","44","55","66","77","88","99","1010","1111","1212","1313","1414","1515"};
         int[]age={11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
         int[]worth={10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,110000,120000,130000,140000,150000};
-        return null;
+        String[]mainsport={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
+        for(int i=0;i<15;i++){
+            Player b=new Player(names[i],age[i],worth[i],mainsport[i],R.drawable.player_one);
+            a[i]=b;
+        }
+        return a;
     }
 }
